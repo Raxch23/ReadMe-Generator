9593bcf3868bfedc803c2ee44c95763fc65ea691
rachiratou Esteve <rachiraesteve@rachiratous-MacBook-Pro.local>
2024-03-06 22:05:56 +0000

    added in the questions, completed writtofile function, completed init function, completed generatemarkdown function

---
+++ README.md
@@ -0,0 +1,31 @@
+# ReadMe-generator
+
+## Description 
+A ReadME generator that can quickly create a professional README for a 
+new project.
+
+## Table of Contents
+- [Installation](#installation)
+- [Usage](#usage)
+- [License](#license)
+- [Contributions](#contributions)
+- [Tests](#tests)
+- [Questions](#questions)
+
+## Installation 
+Run command npm install to have the node dependencies downloaded.
+
+## Usage 
+Run command npm run dev to have a development server up and running.
+
+## License 
+MIT license
+
+## Contributing 
+N/A
+
+## Tests 
+N/A
+
+## Questions
+Reach out at rachiesteve@yahoo.com for any questions.
--- index.js
+++ index.js
@@ -5,16 +5,59 @@ const generateMarkdown = require("./utils/generateMarkdown");
 
 // array of questions for user
 const questions = [
-
+    { 
+        type: 'input',
+        name: 'title',
+        message: 'Project title?:'
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Project description?:'
+    },
+    {
+        type: 'input',
+        name: 'installation',
+        message: 'Installation:'
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'Usage instructions?:'
+    },
+    {
+        type: 'input',
+        name: 'license',
+        message: 'Project license?:'
+    },
+    {
+        type: 'input',
+        name: 'contributing',
+        message: 'How to contribute to this project?:'
+    },
+    {
+        type: 'input',
+        name: 'tests',
+        message: 'How to run tests?:'
+    },
+    {
+        type: 'input',
+        name: 'questions',
+        message: 'Where can people reach out to you if they have any questions about your project?:'
+    },
+    
 ];
 
 // function to write README file
 function writeToFile(fileName, data) {
+    fs.writeFileSync(`${path.resolve()}/output/${fileName}`, data)
 }
 
 // function to initialize program
 function init() {
-
+    inquirer.prompt(questions).then((answers) => {
+        writeToFile('README.md', generateMarkdown(answers))
+    })
 }
 
 // function call to initialize program
